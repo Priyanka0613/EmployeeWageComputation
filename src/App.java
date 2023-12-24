cfd98294a33f00316fc7c4b353ed8e36eb131b44
Priyanka <priyankaag.0613@gmail.com>
2023-12-24 20:25:24 +0530

    UC4-monthly wage

--- src/App.java
+++ src/App.java
@@ -1,8 +1,38 @@
+import java.util.Scanner;
+
 import Employee.Employee;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Employee Wage Computation");
-        Employee.printPresentAbsent();
-        System.out.println("Your Daily Wage is: " + Employee.dailyWage());
+
+        Scanner s=new Scanner(System.in);
+        System.out.println("Press 1 for attendance check");
+        System.out.println("Press 2 for fultime/partime check");
+        System.out.println("Press 3 for daily wage check");
+        System.out.println("Press 4 for monthly wage check");
+        System.out.println("Press 5 for monthly wage under condition check");
+        System.out.println("*************************************************");
+        int option=s.nextInt();
+
+        switch(option){
+            case 1:
+                Employee.printPresentAbsent();
+                break;
+            
+            case 2:
+                Employee.printfulltimeparttime();
+                break;
+
+            case 3:
+                System.out.println("Your Daily Wage is: " + Employee.dailyWage());
+                break;
+            
+            case 4:
+                System.out.println("Your Monthly Wage is: " + Employee.monthlywagecheck());
+                break;
+
+        }
+
+        s.close();
     }
 }
--- src/Employee/Employee.java
+++ src/Employee/Employee.java
@@ -7,8 +7,8 @@ public class Employee {
         return random.nextInt(2);
     }
     static int x=checkPresentAbsent();
+
     public static void printPresentAbsent(){
-        // int x=checkPresentAbsent();
         if(x==0){System.out.println("absent");}
         else{System.out.println("present");}
     }
@@ -18,6 +18,11 @@ public class Employee {
         return random.nextInt(2); //0->parttime; 1->fulltime
     }
 
+    public static void printfulltimeparttime(){
+        if(x==0){System.out.println("part-time");}
+        else{System.out.println("full-time");}
+    }
+
     public static int dailyWage(){
         int fulltimehour=8;
         int parttimehour=4;
@@ -25,6 +30,16 @@ public class Employee {
             return x*20*fulltimehour;
         }else{
             return x*20*parttimehour;
+        }
+    }
+
+    public static int monthlywagecheck(){
+        int fulltimehour=8;
+        int parttimehour=4;
+        if(fulltimeParttime()==1){
+            return 20*x*20*fulltimehour;
+        }else{
+            return 20*x*20*parttimehour;
         }
     }
 }
